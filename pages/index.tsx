bbca0ac7836de84ea7cd199575d010572bfe02fe
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2022-04-06 19:05:10 -0500

    jaja configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,38 +18,36 @@ const Home: NextPage = () => {
         <title>jaja generator</title>
       </Head>
 
-      <main className="mx-auto max-w-5xl px-4 pt-8">
-        <h1 className="p-8 w-full text-5xl font-bold text-center">
+      <main className="mx-auto max-w-3xl px-4 pt-8">
+        <h1 className="p-8 w-full text-4xl font-bold text-center">
           jaja generator
         </h1>
-        <div className="flex flex-row">
-          <div className="w-2/3 p-4 mr-8">
-            <p className="text-2xl mb-8 font-semibold">
-              Number of repetitions: {count}
-            </p>
-            <input
-              type="range"
-              name="repetitions"
-              className="w-full"
-              min="1"
-              max="200"
-              value={count}
-              onChange={(e) => setCount(parseInt(e.target.value))}
-            />
-          </div>
-          <div className="w-1/3 p-4">
-            <p className="text-2xl mb-3 font-semibold">Prefix</p>
-            <input
-              type="text"
-              placeholder="ja"
-              className="text-2xl p-1 outline-none bg-transparent border-b-4 "
-              value={prefix}
-              onChange={(e) => setPrefix(e.target.value)}
-            />
-          </div>
+        <div className="py-1">
+          <p className="text-lg mb-2 font-semibold">
+            Number of repetitions: {count}
+          </p>
+          <input
+            type="range"
+            name="repetitions"
+            className="w-full"
+            min="1"
+            max="500"
+            value={count}
+            onChange={(e) => setCount(parseInt(e.target.value))}
+          />
+        </div>
+        <div className="py-1">
+          <p className="text-lg mb-2 font-semibold">Prefix</p>
+          <input
+            type="text"
+            placeholder="ja"
+            className="text-lg p-1 outline-none bg-transparent border-b"
+            value={prefix}
+            onChange={(e) => setPrefix(e.target.value)}
+          />
         </div>
       </main>
-      <div className="mx-auto max-w-[2000px] bg-graeen-200 mt-10 p-6 break-all text-9xl">
+      <div className="w-full mt-10 py-6 px-10 break-all overflow-scroll text-5xl font-mono">
         {output}
       </div>
     </div>
